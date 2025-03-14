4811f6a831552959103078897d848688ca4df3be
LeviathanCoding <giangjk0000@gmail.com>
2025-03-14 18:27:39 +0700

    lan 2

--- src/sever.js
+++ src/sever.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine.js";
 import initWebRoutes from './route/web.js';
+import connectDB from './config/connectDB.js'
 require('dotenv').config();
 
 
@@ -14,7 +15,7 @@ app.use(bodyParser.urlencoded({extended : true}));
 
 viewEngine(app);
 initWebRoutes(app);
-
+connectDB();
 let port = process.env.PORT || 6969;
 //PORT == undefine => PORT =6969
 app.listen(port, () =>{
